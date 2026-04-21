f57eec3b96d9a7f621954c904417b0331fa66cd0
yahei-DTU <yahei@dtu.dk>
2026-04-21 15:40:25 +0200

    api file added

---
+++ src/day_ahead_v2/api.py
@@ -0,0 +1,64 @@
+from fastapi import FastAPI
+from http import HTTPStatus
+from enum import Enum
+import re
+from contextlib import asynccontextmanager
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Hello")
+    yield
+    print("Goodbye")
+
+app = FastAPI(lifespan=lifespan)
+
+@app.get("/")
+def root():
+    """ Health check."""
+    response = {
+        "message": HTTPStatus.OK.phrase,
+        "status-code": HTTPStatus.OK,
+    }
+    return response
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int):
+    return {"item_id": item_id}
+
+class ItemEnum(Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+@app.get("/restric_items/{item_id}")
+def read_item(item_id: ItemEnum):
+    return {"item_id": item_id}
+
+@app.get("/query_items")
+def read_item(item_id: int):
+    return {"item_id": item_id}
+
+database = {'username': [ ], 'password': [ ]}
+
+@app.post("/login/")
+def login(username: str, password: str):
+    username_db = database['username']
+    password_db = database['password']
+    if username not in username_db and password not in password_db:
+        with open('database.csv', "a") as file:
+            file.write(f"{username}, {password} \n")
+        username_db.append(username)
+        password_db.append(password)
+    return "login saved"
+
+@app.get("/text_model/")
+def contains_email(data: str):
+    regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+    response = {
+        "input": data,
+        "message": HTTPStatus.OK.phrase,
+        "status-code": HTTPStatus.OK,
+        "is_email": re.fullmatch(regex, data) is not None
+    }
+    return response
